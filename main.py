f979c93825978720a3c0a6f645d5c21c1054ed57
Irina Sirbu <irinasirbu@Irinas-MacBook-Air.local>
2025-04-30 21:29:11 +0300

    Changed main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+class Main(BaseModel):
+    name: str
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return Main(name='Sumarizare')
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
